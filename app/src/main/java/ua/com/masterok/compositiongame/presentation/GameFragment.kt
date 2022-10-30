cb755521129a0060203f49780a209074822a0ffd
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-10-30 23:42:04 +0200

    presentation

---
+++ app/src/main/java/ua/com/masterok/compositiongame/presentation/GameFragment.kt
@@ -0,0 +1,19 @@
+package ua.com.masterok.compositiongame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ua.com.masterok.compositiongame.R
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+}
