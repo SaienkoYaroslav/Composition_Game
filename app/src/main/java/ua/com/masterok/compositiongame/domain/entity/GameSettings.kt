287b715812cb85ee39c49490e62eb6c737857759
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-11-02 22:19:40 +0200

    Parcelable

--- app/src/main/java/ua/com/masterok/compositiongame/domain/entity/GameSettings.kt
+++ app/src/main/java/ua/com/masterok/compositiongame/domain/entity/GameSettings.kt
@@ -1,10 +1,13 @@
 package ua.com.masterok.compositiongame.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswer: Int,
     val minPercentOfRightAnswer: Int,
     val gameTimeInSeconds: Int
-) : Serializable
+) : Parcelable
--- app/src/main/java/ua/com/masterok/compositiongame/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/ua/com/masterok/compositiongame/presentation/ChooseLevelFragment.kt
@@ -56,7 +56,6 @@ class ChooseLevelFragment : Fragment() {
 
     companion object {
 
-        const val NAME = "ChooseLevelFragment"
 
         fun newInstance(): ChooseLevelFragment {
             return ChooseLevelFragment()
