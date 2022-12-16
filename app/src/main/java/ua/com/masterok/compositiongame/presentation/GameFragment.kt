6628b6286215a00bb5bdadc3cfb7139a46c4c885
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-12-16 16:20:53 +0200

    GameViewModel Finished

--- app/src/main/java/ua/com/masterok/compositiongame/presentation/GameFragment.kt
+++ app/src/main/java/ua/com/masterok/compositiongame/presentation/GameFragment.kt
@@ -1,15 +1,17 @@
 package ua.com.masterok.compositiongame.presentation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ua.com.masterok.compositiongame.R
 import ua.com.masterok.compositiongame.databinding.FragmentGameBinding
 import ua.com.masterok.compositiongame.domain.entity.GameResult
-import ua.com.masterok.compositiongame.domain.entity.GameSettings
 import ua.com.masterok.compositiongame.domain.entity.Level
 
 class GameFragment : Fragment() {
@@ -19,7 +21,26 @@ class GameFragment : Fragment() {
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     private lateinit var level: Level
-    private lateinit var viewModel: GameViewModel
+
+    // Лінива ініціалізація (змінна ініціалізується при першому зверненні до неї, до цього вона не ініціалізована)
+    // Змінна оголошується як val, вкцінці пишеться by lazy { тут пишемо ініціалізацію }
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[GameViewModel::class.java]
+    }
+
+    private val tvOptions by lazy {
+        mutableListOf<TextView>().apply {
+            add(binding.tvOption1)
+            add(binding.tvOption2)
+            add(binding.tvOption3)
+            add(binding.tvOption4)
+            add(binding.tvOption5)
+            add(binding.tvOption6)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,26 +57,9 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
-        viewModel.countDownTimer(level)
-        viewModel.timer.observe(viewLifecycleOwner) {
-            val secs = it
-            val formatted = "${(secs / 60).toString().padStart(2, '0')}:${(secs % 60).toString().padStart(2, '0')}"
-            binding.tvTimer.text = formatted
-        }
-        viewModel.isTimerFinished.observe(viewLifecycleOwner) {
-            if (it) {
-                launchGameFinishedFragment(
-                    GameResult(
-                    false,
-                    10,
-                    10,
-                    GameSettings(10,2,3,2)
-                )
-                )
-            }
-        }
+        observeViewModel()
+        setClickListenersToOptions()
+        viewModel.startGame(level)
     }
 
     override fun onDestroyView() {
@@ -72,8 +76,65 @@ class GameFragment : Fragment() {
     private fun launchGameFinishedFragment(gameResult: GameResult) {
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null )
+            .addToBackStack(null)
             .commit()
+    }
+
+    private fun setClickListenersToOptions() {
+        for (tvOptions in tvOptions) {
+            tvOptions.setOnClickListener {
+                viewModel.chooseAnswer(tvOptions.text.toString().toInt())
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumber.toString()
+            for (i in 0 until tvOptions.size) {
+                tvOptions[i].text = it.options[i].toString()
+            }
+        }
+
+        viewModel.percentOfRightAnswer.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+
+        viewModel.enoughCountOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.tvAnswersProgress.setTextColor(getColorByState(it))
+        }
+
+        viewModel.enoughPercentOfRightAnswers.observe(viewLifecycleOwner) {
+            val color = getColorByState(it)
+            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
+        }
+
+        viewModel.formattedTime.observe(viewLifecycleOwner) {
+            binding.tvTimer.text = it
+        }
+
+        viewModel.minPercent.observe(viewLifecycleOwner) {
+            binding.progressBar.secondaryProgress = it
+        }
+
+        viewModel.gameResult.observe(viewLifecycleOwner) {
+            launchGameFinishedFragment(it)
+        }
+
+        viewModel.progressAnswers.observe(viewLifecycleOwner) {
+            binding.tvAnswersProgress.text = it
+        }
+
+    }
+
+    private fun getColorByState(goodState: Boolean): Int {
+        val colorResId = if (goodState) {
+            android.R.color.holo_green_light
+        } else {
+            android.R.color.holo_red_light
+        }
+        return ContextCompat.getColor(requireContext(), colorResId)
     }
 
     companion object {
--- app/src/main/java/ua/com/masterok/compositiongame/presentation/GameViewModel.kt
+++ app/src/main/java/ua/com/masterok/compositiongame/presentation/GameViewModel.kt
@@ -1,44 +1,158 @@
 package ua.com.masterok.compositiongame.presentation
 
+import android.app.Application
 import android.os.CountDownTimer
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import ua.com.masterok.compositiongame.R
 import ua.com.masterok.compositiongame.data.GameRepositoryImpl
+import ua.com.masterok.compositiongame.domain.entity.GameResult
 import ua.com.masterok.compositiongame.domain.entity.GameSettings
 import ua.com.masterok.compositiongame.domain.entity.Level
+import ua.com.masterok.compositiongame.domain.entity.Question
+import ua.com.masterok.compositiongame.domain.usecases.GenerateQuestionsUseCase
+import ua.com.masterok.compositiongame.domain.usecases.GetGameSettingsUseCase
 
-class GameViewModel : ViewModel() {
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+
+    private lateinit var gameSettings: GameSettings
+    private lateinit var level: Level
+
+    private val context = application
 
     private val repository: GameRepositoryImpl = GameRepositoryImpl
 
-    private var _timer = MutableLiveData<Int>()
-    val timer: LiveData<Int>
-    get() = _timer
+    private val generateQuestionUseCase = GenerateQuestionsUseCase(repository)
+    private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
 
-    private val _isTimerFinished = MutableLiveData<Boolean>()
-    val isTimerFinished: LiveData<Boolean>
-    get() = _isTimerFinished
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime: LiveData<String>
+        get() = _formattedTime
+
+    private var timer: CountDownTimer? = null
+
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = _question
 
 
-    fun countDownTimer(level: Level) {
-        val gameMode = getGameSettings(level)
-        val maxTimeInMill = (gameMode.gameTimeInSeconds * 1000).toLong()
-        val timer = object : CountDownTimer(maxTimeInMill, 1000) {
+    private val _percentOfRightAnswer = MutableLiveData<Int>()
+    val percentOfRightAnswer: LiveData<Int>
+        get() = _percentOfRightAnswer
+
+    private val _progressAnswers = MutableLiveData<String>()
+    val progressAnswers: LiveData<String>
+        get() = _progressAnswers
+
+    private val _enoughCountOfRightAnswers = MutableLiveData<Boolean>()
+    val enoughCountOfRightAnswers: LiveData<Boolean>
+        get() = _enoughCountOfRightAnswers
+
+    private val _enoughPercentOfRightAnswers = MutableLiveData<Boolean>()
+    val enoughPercentOfRightAnswers: LiveData<Boolean>
+        get() = _enoughPercentOfRightAnswers
+
+    private val _minPercent = MutableLiveData<Int>()
+    val minPercent: LiveData<Int>
+        get() = _minPercent
+
+    private val _gameResult = MutableLiveData<GameResult>()
+    val gameResult: LiveData<GameResult>
+        get() = _gameResult
+
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
+
+    fun startGame(level: Level) {
+        getGameSettings(level)
+        startTimer()
+        generateQuestion()
+        updateProgress()
+    }
+
+    private fun getGameSettings(level: Level) {
+        this.level = level
+        this.gameSettings = getGameSettingsUseCase(level)
+        _minPercent.value = gameSettings.minPercentOfRightAnswer
+    }
+
+    private fun startTimer() {
+        val maxTimeInMill = (gameSettings.gameTimeInSeconds * MILLIS_IN_SECONDS)
+        timer = object : CountDownTimer(maxTimeInMill, MILLIS_IN_SECONDS) {
             override fun onTick(millisUntilFinished: Long) {
-                _timer.value = (millisUntilFinished/1000).toInt()
+                _formattedTime.value = formatTime(millisUntilFinished)
             }
 
             override fun onFinish() {
-                _isTimerFinished.value = true
+                finishGame()
             }
 
         }
-        timer.start()
+        timer?.start()
     }
 
-    private fun getGameSettings(level: Level): GameSettings {
-         return repository.getGameSettings(level)
+    private fun formatTime(millisUntilFinished: Long): String {
+        val secs = (millisUntilFinished / MILLIS_IN_SECONDS).toInt()
+        val formatted =
+            "${(secs / 60).toString().padStart(2, '0')}:${(secs % 60).toString().padStart(2, '0')}"
+        return formatted
+    }
+
+    private fun finishGame() {
+        _gameResult.value = GameResult(
+            enoughCountOfRightAnswers.value == true && enoughPercentOfRightAnswers.value == true,
+            countOfRightAnswers,
+            countOfQuestions,
+            gameSettings
+        )
+    }
+
+    private fun generateQuestion() {
+        _question.value = generateQuestionUseCase(gameSettings.maxSumValue)
+    }
+
+    fun chooseAnswer(number: Int) {
+        checkAnswer(number)
+        updateProgress()
+        generateQuestion()
+    }
+
+    private fun checkAnswer(number: Int) {
+        val rightAnswer = question.value?.rightAnswer
+        if (number == rightAnswer) {
+            countOfRightAnswers++
+        }
+        countOfQuestions++
+    }
+
+    private fun updateProgress() {
+        val percent = calculatePercentOfRightAnswer()
+        _percentOfRightAnswer.value = percent
+        _progressAnswers.value = String.format(
+            context.resources.getString(R.string.progress_answers),
+            countOfRightAnswers,
+            gameSettings.minCountOfRightAnswer
+        )
+        _enoughCountOfRightAnswers.value = countOfRightAnswers >= gameSettings.minCountOfRightAnswer
+        _enoughPercentOfRightAnswers.value = percent >= gameSettings.minPercentOfRightAnswer
+    }
+
+    private fun calculatePercentOfRightAnswer(): Int {
+        if (countOfQuestions == 0) {
+            return 0
+        }
+        return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+
+
+    companion object {
+        private const val MILLIS_IN_SECONDS = 1000L
     }
 
 }
