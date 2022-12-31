0905660af3f43d24f1a48c551890fad3f4ed3483
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-12-31 16:05:10 +0200

    data binding and viewModel & clickListener

--- app/src/main/java/ua/com/masterok/compositiongame/presentation/BindingAdapters.kt
+++ app/src/main/java/ua/com/masterok/compositiongame/presentation/BindingAdapters.kt
@@ -1,10 +1,19 @@
 package ua.com.masterok.compositiongame.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import ua.com.masterok.compositiongame.R
 import ua.com.masterok.compositiongame.domain.entity.GameResult
+
+interface OnOptionClickListener {
+
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -51,10 +60,47 @@ fun bindEmojiResult(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(getEmoticon(winner))
 }
 
-    private fun getEmoticon(winner: Boolean): Int {
-        return if (winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
+private fun getEmoticon(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
     }
+}
+
+@BindingAdapter("progressBar")
+fun bindProgressBar(progressBar: ProgressBar, count: Int) {
+    progressBar.setProgress(count, true)
+}
+
+@BindingAdapter("colorCountOfRightAnswers")
+fun bindColorCountOfRightAnswers(textView: TextView, goodState: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, goodState))
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("colorProgressBar")
+fun bindColorProgressBar(progressBar: ProgressBar, goodState: Boolean) {
+    val color = getColorByState(progressBar.context, goodState)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
